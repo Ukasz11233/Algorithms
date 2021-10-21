daaf594728737a57856f7cb142ccbbd12637270b
Ukasz11233 <56895986+Ukasz11233@users.noreply.github.com>
2021-10-21 14:58:41 +0200

    Delete Algorytmy directory

--- Algorytmy/Kolokwia_I/2017_zad3.py
+++
@@ -1,4 +0,0 @@
-#Tworzymy tablice wielkosci: n-k+1. Przechodzimy przez ciag liter, i wrzucamy do stworzonej tablicy
-#wszystkie podciagu dlugosci k. Sortujemy stworzona tablice z podciagami, np leksykograficznie.
-#Na koniec przechodzimy raz po tablicy, aby znalezc najdluzszy podciag takich samych liter dlugosci k.
-#Zlozonosc to: O(n + n + n) = O(n)
